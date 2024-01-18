054c66e526fc3ca8dc971a02d40131b855000e0d
Theonethinker <156123753+Theonethinker@users.noreply.github.com>
2024-01-18 12:33:11 -0600

    Restore '1/18/2024, 12:29:54 PM Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.setBackgroundColor(1)
-let mySprite = sprites.create(img`
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . 5 5 5 5 5 5 . . . . 
@@ -17,24 +17,7 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
-splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, mySprite)
+    `, SpriteKind.Player))
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, null)
 splitScreen.setCameraRegion(splitScreen.Camera.Camera1, splitScreen.CameraRegion.VerticalLeftHalf)
-let Enemy1 = sprites.create(img`
-    . . . . f f f f f . . . . . . . 
-    . . . f e e e e e f . . . . . . 
-    . . f d d d d e e e f . . . . . 
-    . c d f d d f d e e f f . . . . 
-    . c d f d d f d e e d d f . . . 
-    c d e e d d d d e e b d c . . . 
-    c d d d d c d d e e b d c . f f 
-    c c c c c d d d e e f c . f e f 
-    . f d d d d d e e f f . . f e f 
-    . . f f f f f e e e e f . f e f 
-    . . . . f e e e e e e e f f e f 
-    . . . f e f f e f e e e e f f . 
-    . . . f e f f e f e e e e f . . 
-    . . . f d b f d b f f e f . . . 
-    . . . f d d c d d b b d f . . . 
-    . . . . f f f f f f f f f . . . 
-    `, SpriteKind.Enemy)
