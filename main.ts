8de7d51fcf6f65606c567ffaba9f2b9f39cbf9ae
Theonethinker <156123753+Theonethinker@users.noreply.github.com>
2024-01-18 12:30:49 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -20,3 +20,21 @@ let mySprite = sprites.create(img`
     `, SpriteKind.Player)
 splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, mySprite)
 splitScreen.setCameraRegion(splitScreen.Camera.Camera1, splitScreen.CameraRegion.VerticalLeftHalf)
+let Enemy1 = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `, SpriteKind.Enemy)
