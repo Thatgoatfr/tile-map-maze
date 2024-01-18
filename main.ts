1a9edfa5f5a19aac5c43251a09858df1982ad599
Thatgoatfr <150864315+Thatgoatfr@users.noreply.github.com>
2024-01-18 11:19:00 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
 tiles.setCurrentTilemap(tilemap`level1`)
+scene.setBackgroundColor(1)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 5 5 5 5 5 5 . . . . 
+    . . . . . 5 5 5 5 5 5 5 5 . . . 
+    . . . . . . f f f f f . . . . . 
+    . . . . . . f e f e f . . . . . 
+    . . . . . . f f f f f . . . . . 
+    . . . . . . f f f f f . . . . . 
+    . . . . . f 5 5 5 5 5 f . . . . 
+    . . . . . f 5 5 5 5 5 f . . . . 
+    . . . . . . 5 5 5 5 5 . . . . . 
+    . . . . . . f f . f f . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
