7173d020ac8d345ee1175a94c942831fe7c4f557
Thatgoatfr <150864315+Thatgoatfr@users.noreply.github.com>
2024-01-22 19:05:11 -0600

    Update pxt.json, main.blocks, main.ts, README.md, assets.json, tilemap.g.jres, tilemap.g.ts, test.ts

---
+++ main.ts
@@ -0,0 +1,156 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
+function animaRUN() {
+    mainRunLeft = animation.createAnimation(ActionKind.Walking, 1000)
+    animation.attachAnimation(mySprite, mainRunLeft)
+    mainRunLeft.addAnimationFrame(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 5 5 5 5 . . . . . . . . 
+        . . . 5 5 5 5 5 5 . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . . e f f f f . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . f 5 5 5 5 5 f . . . . . . 
+        . . . f 5 5 5 5 5 f . . . . . . 
+        . . . . 5 5 5 5 5 . . . . . . . 
+        . . . . f f . f f . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    mainRunLeft.addAnimationFrame(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 5 5 5 5 . . . . . . . . 
+        . . . 5 5 5 5 5 5 . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . . e f f f f . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . f f 5 5 5 5 5 f . . . . . . 
+        . . . . 5 5 5 5 5 f . . . . . . 
+        . . . . 5 5 5 5 5 . . . . . . . 
+        . . . . f f . f f . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    mainRunLeft.addAnimationFrame(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 5 5 5 5 . . . . . . . . 
+        . . . 5 5 5 5 5 5 . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . . e f f f f . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . f 5 5 5 5 5 f f . . . . . 
+        . . . f 5 5 5 5 5 . . . . . . . 
+        . . . . 5 5 5 5 5 . . . . . . . 
+        . . . . f f . f f . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+}
+let mainRunLeft: animation.Animation = null
+let mySprite: Sprite = null
+tiles.setCurrentTilemap(tilemap`level1`)
+scene.setBackgroundColor(1)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 5 5 5 5 5 5 . . . . 
+    . . . . . 5 5 5 5 5 5 5 5 . . . 
+    . . . . . . f f f f f . . . . . 
+    . . . . . . f e f e f . . . . . 
+    . . . . . . f f f f f . . . . . 
+    . . . . . . f f f f f . . . . . 
+    . . . . . f 5 5 5 5 5 f . . . . 
+    . . . . . f 5 5 5 5 5 f . . . . 
+    . . . . . . 5 5 5 5 5 . . . . . 
+    . . . . . . f f . f f . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), mySprite)
+let mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 7 7 7 7 7 7 . . . . 
+    . . . . . 7 7 7 7 7 7 7 7 . . . 
+    . . . . . . e e e e e . . . . . 
+    . . . . . . e 1 e 1 e . . . . . 
+    . . . . . . e e e e e . . . . . 
+    . . . . . . e e e e e . . . . . 
+    . . . . . e 7 7 7 7 7 e . . . . 
+    . . . . . e 7 7 7 7 7 e . . . . 
+    . . . . . . 7 7 7 7 7 . . . . . 
+    . . . . . . e e . e e . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), mySprite2)
+let mySprite3 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 6 6 6 6 6 6 . . . . 
+    . . . . . 6 6 6 6 6 6 6 6 . . . 
+    . . . . . . 4 4 4 4 4 . . . . . 
+    . . . . . . 4 8 4 8 4 . . . . . 
+    . . . . . . 4 4 4 4 4 . . . . . 
+    . . . . . . 4 4 4 4 4 . . . . . 
+    . . . . . 4 6 6 6 6 6 4 . . . . 
+    . . . . . 4 6 6 6 6 6 4 . . . . 
+    . . . . . . 6 6 6 6 6 . . . . . 
+    . . . . . . 4 4 . 4 4 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), mySprite3)
+let mySprite4 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 8 8 8 8 8 8 . . . . 
+    . . . . . 8 8 8 8 8 8 8 8 . . . 
+    . . . . . . 7 7 7 7 7 . . . . . 
+    . . . . . . 7 f 7 f 7 . . . . . 
+    . . . . . . 7 7 7 7 7 . . . . . 
+    . . . . . . 7 7 7 7 7 . . . . . 
+    . . . . . 7 8 8 8 8 8 7 . . . . 
+    . . . . . 7 8 8 8 8 8 7 . . . . 
+    . . . . . . 8 8 8 8 8 . . . . . 
+    . . . . . . 7 7 . 7 7 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), mySprite4)
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two))
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Three))
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Four))
+splitScreen.setCameraRegion(splitScreen.Camera.Camera1, splitScreen.CameraRegion.TopLeft)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, mySprite)
+splitScreen.setBorderColor(8)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera2, mySprite2)
+splitScreen.setCameraRegion(splitScreen.Camera.Camera2, splitScreen.CameraRegion.TopRight)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera3, mySprite3)
+splitScreen.setCameraRegion(splitScreen.Camera.Camera3, splitScreen.CameraRegion.BottomLeft)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera4, mySprite4)
+splitScreen.setCameraRegion(splitScreen.Camera.Camera4, splitScreen.CameraRegion.BottomRight)
